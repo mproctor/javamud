6c6a522d524ab4d3b1945a7f7e94c5e99892c7a3
Matt Proctor <matt@theproctors.net>
2011-03-28 22:35:56 +0100

    init from map

--- src/main/java/javamud/command/DefaultCommandParser.java
+++ src/main/java/javamud/command/DefaultCommandParser.java
@@ -1,10 +1,25 @@
 package javamud.command;
 
-import javamud.data.DefaultTrie;
 import javamud.data.Trie;
+import java.util.*;
 
 public class DefaultCommandParser implements CommandParser {
-	private Trie t = new DefaultTrie();
+	private Trie t;
+	private Map cmdMap;
+	
+	public void setT(Trie t) {
+		this.t = t;
+	}
+	
+	public void init() {
+		t.init(cmdMap);
+		
+		// TODO: clear cmdMap for garbage collection?
+	}
+
+	public void setCmdMap(Map cmdMap) {
+		this.cmdMap = cmdMap;
+	}
 
 	@Override
 	public Command parse(String cmd) {
